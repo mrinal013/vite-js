3afe89228710ec12fda10c5a6e5e02e008220cba
Mrinal Haque <mrinalhaque99@gmail.com>
2022-10-29 20:05:59 +0600

    vuetifychat app init

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import 'vuetify/styles' // Global CSS has to be imported
+import { createApp } from 'vue'
+import { createVuetify } from 'vuetify'
+import App from './App.vue'
+
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+const app = createApp(App)
+// const vuetify = createVuetify() // Replaces new Vuetify()
+
+app.use(vuetify)
+
+app.mount('#app')
