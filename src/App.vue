95c4d9d8af7d3295bcaa72325df973de7732a1f7
Mrinal Haque <mrinalhaque99@gmail.com>
2022-10-29 20:57:38 +0600

    very basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <template>
   <v-app>
+    
+    <v-app-bar color="primary">
+      <v-app-bar-title>Chatting App</v-app-bar-title>
+      <v-spacer></v-spacer>
+      <v-btn variant="outlined">Start</v-btn>
+    </v-app-bar>
+
     <v-main>
-      <HelloWorld/>
+      <v-container>
+        <p>Hello chat app</p>
+      </v-container>
     </v-main>
+
+    <v-footer class="bg-indigo" app>
+      chat box
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
   },
 
   data: () => ({
